c6fda3e9fc530cb5ff2ee6d083be80389c5c000c
DinhLN <lenhatdinh@gmail.com>
2026-05-09 11:43:54 +0700

    Fixed

---
+++ test/test-logger.js
@@ -0,0 +1,16 @@
+const { createLogger } = require("../core/logger");
+
+const log = createLogger("trace-tx");
+
+async function simulateError() {
+  try {
+    throw new Error("TEST_ERROR: day la loi thu nghiem tu trace-tx");
+  } catch (err) {
+    log.error(`Loi tx 0xTEST: ${err.message}`);
+  }
+}
+
+simulateError().then(() => {
+  console.log("Test xong. Kiem tra pm2 logs trace-tx");
+  process.exit(0);
+});
